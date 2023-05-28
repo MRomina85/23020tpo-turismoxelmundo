8563105d9fd23d2e93441b613bebee5fe83243e7
starligghtty <smsc5551@gmail.com>
2023-05-28 14:58:16 -0300

    Eliminé estructuras que estaban comentadas

--- js/hoteles.js
+++ js/hoteles.js
@@ -24,67 +24,12 @@ createApp({
                     console.log(data)
                     this.hotels = data.hotels
                     this.hotelesALL = data.hotels
-                    //this.cargarListasDesplegables()
+                    
 
                 })
                 .catch(error => alert("Ups... se produjo un error: " + error))
         },
       
-        
-        /*cargarListasDesplegables() {
-            this.countryAsia = []
-            this.countryEurope = []
-            this.countryAmerica = []
-            this.countryAfrica = []
-
-            for (elemento of this.hotelesALL) {
-
-                if (elemento.continent_name == "Asia") {
-
-                    if (elemento.country != '' && this.countryAsia.indexOf(elemento.country)) {
-                        this.countryAsia.push(elemento.country)
-                    }
-
-                    if (elemento.city != '' && this.city.indexOf(elemento.city)) {
-                        this.city.push(elemento.city)
-                    }
-
-                }
-                if (elemento.continent_name == "Europe") {
-
-                    if (elemento.country != '' && this.countryEurope.indexOf(elemento.country)) {
-                        this.countryEurope.push(elemento.country)
-                    }
-
-                    if (elemento.city != '' && this.city.indexOf(elemento.city)) {
-                        this.city.push(elemento.city)
-                    }
-
-                }
-                if (elemento.continent_name == "South America" ||elemento.continent_name =="North America") {
-
-                    if (elemento.country != '' && this.countryAmerica.indexOf(elemento.country)) {
-                        this.countryAmerica.push(elemento.country)
-                    }
-
-                    if (elemento.city != '' && this.city.indexOf(elemento.city)) {
-                        this.city.push(elemento.city)
-                    }
-
-                }
-                if (elemento.continent_name == "Africa") {
-
-                    if (elemento.country != '' && this.countryAfrica.indexOf(elemento.country)) {
-                        this.countryAfrica.push(elemento.country)
-                    }
-
-                    if (elemento.city != '' && this.city.indexOf(elemento.city)) {
-                        this.city.push(elemento.city)
-                    }
-                }
-            }
-        }*/
-
     },
     created() {
         this.fetchData(this.url)                                                         
